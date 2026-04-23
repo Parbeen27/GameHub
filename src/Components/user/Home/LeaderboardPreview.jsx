90d92f20609817cc8c5d77e968125359a6ee060c
Parbeen27 <parbeensingh27@gmail.com>
2026-04-23 19:37:08 +0530

    Import api service in LeaderboardPreview component

--- src/Components/user/Home/LeaderboardPreview.jsx
+++ src/Components/user/Home/LeaderboardPreview.jsx
@@ -2,6 +2,7 @@ import axios from 'axios'
 import React from 'react'
 import { useEffect } from 'react'
 import { useState } from 'react'
+import {api} from "../../../services/api"
 
 export default function LeaderboardPreview() {
     const [leaders, setLeaders] = useState([])
